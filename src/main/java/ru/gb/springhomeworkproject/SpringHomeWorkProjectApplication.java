9a1e2a754b1be6d47ee21188455ff9e0c696f449
azama <a.kurmanov@csi.kz>
2022-10-31 21:17:59 +0600

    Добавление проекта в Git

---
+++ src/main/java/ru/gb/springhomeworkproject/SpringHomeWorkProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.springhomeworkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeWorkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomeWorkProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/springhomeworkproject/SpringHomeWorkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.springhomeworkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeWorkProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
